92b52f03412ddf1e77173c96f3e66bb38c10c91b
Scott Taylor <scottntaylor21@gmail.com>
2020-02-16 19:06:39 -0800

    Template set up

--- online-marketplace-st/src/App.js
+++ online-marketplace-st/src/App.js
@@ -2,14 +2,22 @@ import React, {Component} from 'react';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import Navbar from "../src/components/Navbar";
 import Home from "../src/components/Home/home";
+import Cart from "../src/components/Cart/cart"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
-
-        <Navbar></Navbar>
+      <BrowserRouter>
+            <div className="App">
+            
+              <Navbar/>
+                <Switch>
+                    <Route exact path="/" component={Home}/>
+                    <Route path="/cart" component={Cart}/>
+                  </Switch>
+             </div>
+       </BrowserRouter>
       </header>
     </div>
   );
--- online-marketplace-st/src/components/Home/home.js
+++ online-marketplace-st/src/components/Home/home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux'
+import { connect } from 'react-redux';
+import { addToCart } from '../CartActions/cartActions';
 
 class Home extends Component {
 
